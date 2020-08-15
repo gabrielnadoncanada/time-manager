f518ced548735903b727282818c3958db135d7ce
GitHub Action <action@github.com>
2020-08-15 17:20:28 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/gabrielnadoncanada/timemanager/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.gabrielnadoncanada.timemanager.services
+
+import com.github.gabrielnadoncanada.timemanager.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/gabrielnadoncanada/timemanager/services/MyProjectService.kt
+++ src/main/kotlin/com/github/gabrielnadoncanada/timemanager/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.gabrielnadoncanada.timemanager.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.gabrielnadoncanada.timemanager.MyBundle
 
 class MyProjectService(project: Project) {
 
